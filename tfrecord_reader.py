831c2b178e7b6079ebde843d538aef67e42897a5
Vijay <vijayvee@gmail.com>
2018-02-11 22:21:47 -0500

    Training started. Change script for mice behaviors

--- tfrecord_reader.py
+++ tfrecord_reader.py
@@ -5,20 +5,19 @@ import numpy as np
 import sys
 import os
 
-def get_video_label_tfrecords(filename_queue,batch_size,shuffle=False):
-    feature = {'val/video': tf.FixedLenFeature([], tf.string),
-              'val/label': tf.FixedLenFeature([], tf.int64)}
+def get_video_label_tfrecords(filename_queue,batch_size,subset,shuffle=False):
+    feature = {'{}/video'.format(subset): tf.FixedLenFeature([], tf.string),
+              '{}/label'.format(subset): tf.FixedLenFeature([], tf.int64)}
     # Define a reader and read the next record
     reader = tf.TFRecordReader()
     _, serialized_example = reader.read(filename_queue)
     # Decode the record read by the reader
     features = tf.parse_single_example(serialized_example, features=feature)
     # Cast label data into int32
-    label = tf.cast(features['val/label'], tf.int32)
-    video_dec = tf.decode_raw(features['val/video'],np.float32)
+    label = tf.cast(features['{}/label'.format(subset)], tf.int64)
+    video_dec = tf.decode_raw(features['{}/video'.format(subset)],np.float32)
     # Reshape image data into the original shape
-    #print image.get_shape().as_list()
-    video = tf.reshape(video_dec, [79, 224, 224, 3])
+    video = tf.reshape(video_dec, [16, 224, 224, 3])
     # Creates batches by randomly shuffling tensors
     if shuffle:
         videos, labels = tf.train.shuffle_batch([video, label], batch_size=batch_size, capacity=30, num_threads=100, min_after_dequeue=10)
@@ -29,7 +28,7 @@ def get_video_label_tfrecords(filename_queue,batch_size,shuffle=False):
 def test_tfrecord_read(tfrecords_filename):
     with tf.Session().as_default() as sess:
         filename_queue = tf.train.string_input_producer([tfrecords_filename], num_epochs=1)
-        videos,labels = get_video_label_tfrecords(filename_queue,3)
+        videos,labels = get_video_label_tfrecords(filename_queue,64,subset='train')
         init_op = tf.group(tf.global_variables_initializer(),
         tf.local_variables_initializer())
         sess.run(init_op)
@@ -38,7 +37,7 @@ def test_tfrecord_read(tfrecords_filename):
         videos_batch,labels_batch = sess.run([videos,labels])
         print videos_batch.shape,labels_batch
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"]='7'
+    os.environ["CUDA_VISIBLE_DEVICES"]='2'
     test_tfrecord_read(sys.argv[1])
 
 if __name__=="__main__":
